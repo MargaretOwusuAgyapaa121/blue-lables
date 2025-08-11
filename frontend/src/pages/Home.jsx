4483831f2c0135e75473292b918ec7fbb2d066f5
MargaretOwusuAgyapaa121 <margarermensah123@gmail.com>
2025-08-12 00:03:21 +0100

    updated my hero video

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,10 +9,22 @@ const Home = () => {
 
       {/* Hero Section */}
       <header className="hero-section">
-        <video autoPlay muted loop className="hero-video">
+        {/* <video autoPlay muted loop className="hero-video">
+          <source src="/assets/images/video/video1.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video> */}
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          className="hero-video"
+          poster="/assets/images/video/fallback.jpg" // optional fallback image
+        >
           <source src="/assets/images/video/video1.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
+
 
 
         <div className="hero-overlay" />
